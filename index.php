acb13844128a48c98ab1152cda01ddd9c2cf2d7c
Someone <rgbjkbs@hdbf.ru>
2018-03-23 20:04:58 +0200

    index.php and gitignor

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>єтот проект мой)))0 Точно узнаю))))</p>
+    </body>
+</html>
